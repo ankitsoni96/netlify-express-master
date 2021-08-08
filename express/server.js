cb4b6f2d69201af1890e4a7401412c38b18758fc
ankitsoni96 <ankitvsoni1996@gmail.com>
2021-08-08 14:18:37 +0530

    aaaa

--- express/server.js
+++ express/server.js
@@ -28,4 +28,6 @@ app.use('/.netlify/functions/server', router);  // path must route to lambda
 // app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app,()=>{
+    DB.connect()
+});
